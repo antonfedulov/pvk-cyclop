f856d95e5dae0ab5fc1fd11aed296040a079f477
Антон Федулов <fedulov.anton@outlook.com>
2024-08-16 19:48:25 +0300

    sdhgigiuhhjjkljkljkljkk

--- src/index.jsx
+++ src/index.jsx
@@ -3,12 +3,62 @@ import ReactDOM from 'react-dom/client';
 import './index.scss';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#bf8426',
+    },
+    secondary: {
+      main: '#dc004e',
+    },
+    background: {
+      default: '#dc004e',
+    },
+    text: {
+      primary: '#1C212C',
+    },
+  },
+  components: {
+    MuiFilledInput: {
+      styleOverrides: {
+        root: {
+          backgroundColor: '#e0f7fa',
+          '&:hover': {
+            backgroundColor: '#b2ebf2',
+          },
+          '&.Mui-focused': {
+            backgroundColor: '#80deea',
+          },
+        },
+      },
+    },
+    MuiSelect: {
+      styleOverrides: {
+        root: {
+          backgroundColor: '#e0f7fa',
+          '&:hover': {
+            backgroundColor: '#b2ebf2',
+          },
+          '&.Mui-focused': {
+            backgroundColor: '#80deea',
+          },
+        },
+      },
+    }
+  }
+});
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <ThemeProvider theme={theme}>
+      <CssBaseline />
+        <App />
+      </ThemeProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
--- src/pages/main/Main.jsx
+++ src/pages/main/Main.jsx
@@ -1,9 +1,69 @@
 import './Main.scss';
+import {TextField, Button, Select, MenuItem} from '@mui/material';
+import React, { useState, useEffect } from 'react';
+
+// import { useNavigate, useLocation } from 'react-router-dom';
 
 export default function Main() {
+
+  const [formData, setFormData] = useState({
+    name: '',
+    operationType: 1,
+    ammoType: '',
+    ammoCount: NaN,
+    remainingAmmoCount: NaN
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
   return (
     <div className='main-page page'>
-      csdssdad
+      <div className='main-page-form'>
+        <TextField
+          label="Назва МВГ"
+          variant="filled"
+          name="name"
+          value={formData.name}
+          onChange={handleChange}
+        />
+        <Select
+          label="Тип операції"
+          name="operationType"
+          value={formData.operationType}
+          onChange={handleChange}
+        >
+          <MenuItem value={1}>розхід набоїв</MenuItem>
+          <MenuItem value={2}>прихід набоїв</MenuItem>
+        </Select>
+        <TextField
+          label="Тип набоїв"
+          variant="filled"
+          name="ammoType"
+          value={formData.ammoType}
+          onChange={handleChange}
+        />
+        <TextField
+          label="Кількість витрачених набоїв"
+          variant="filled"
+          name="ammoCount"
+          type="number"
+          value={formData.ammoCount}
+          onChange={handleChange}
+        />
+        <TextField
+          label="Залишок набоїв"
+          variant="filled"
+          name="remainingAmmoCount"
+          type="number"
+          value={formData.remainingAmmoCount}
+          onChange={handleChange}
+        />
+        <Button variant="contained" color="primary">Відправити звіт</Button>
+      </div>
+      
     </div>
   );
 };
